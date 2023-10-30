822f0f72fe4649e795cee4a3525bb6811c683790
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-10-30 11:57:17 +0530

    transaction

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,71 +1,100 @@
 const Expense = require('../models/expenses');
 const User = require('../models/users');
+const sequelize= require('../util/database');
 
+const addExpense = async (req, res) => {
+    try {
+        const t = await sequelize.transaction();
 
-const addExpense = (req, res) => {
-    const { amount, description, category } = req.body;
-    console.log('Request body:', req.body);
+        const { amount, description, category } = req.body;
+        console.log('Request body:', req.body);
 
-    if (amount === undefined || description === undefined || category === undefined) {
-        return res.status(400).json({ success: false, message: 'Parameters missing' });
+        if (amount === undefined || description === undefined || category === undefined) {
+            return res.status(400).json({ success: false, message: 'Parameters missing' });
+        }
+
+        const expense = await Expense.create(
+            { amount, description, category, userId: req.user.id },
+            { transaction: t }
+        );
+
+        const totalexpenses = Number(req.user.totalexpenses) + Number(amount);
+        console.log(totalexpenses);
+
+        await User.update(
+            { totalexpenses: totalexpenses },
+            { where: { id: req.user.id }, transaction: t }
+        );
+
+        await t.commit();
+        return res.status(201).json({ success: true, expense });
+    } catch (err) {
+        if (t) {
+            await t.rollback();
+        }
+        console.error(err);
+        return res.status(500).json({ success: false, error: err.message || 'Operation failed' });
     }
-
-    Expense.create({ amount, description, category, userId: req.user.id })
-        .then(expense => {
-            const totalexpenses = Number(req.user.totalexpenses) + Number(amount);
-            console.log(totalexpenses);
-            User.update(
-                { totalexpenses: totalexpenses },
-                { where: { id: req.user.id } }
-            )
-                .then(() => {
-                    return res.status(201).json({ success: true, expense });
-                })
-                .catch(err => {
-                    console.error(err);
-                    return res.status(500).json({ success: false, error: 'Failed to update total expenses for the user' });
-                });
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ success: false, error: 'Failed to create expense' });
-        });
-};
-
-const getExpenses = (req, res) => {
-    req.user
-        .getExpenses()
-        .then(expenses => {
-            return res.status(200).json({ success: true, expenses });
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ success: false, error: 'Failed to fetch expenses' });
-        });
 };
 
 
-const deleteExpense = (req, res) => {
+const getExpenses = async (req, res) => {
+    try {
+        const expenses = await req.user.getExpenses();
+        return res.status(200).json({ success: true, expenses });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, error: 'Failed to fetch expenses' });
+    }
+};
+
+
+
+const deleteExpense = async (req, res) => {
     const expenseId = req.params.expenseid;
-    console.log("expenseId = ",req.params.expenseid)
 
     if (expenseId === undefined || expenseId.length === 0) {
         return res.status(400).json({ success: false, message: 'Invalid parameters' });
     }
 
-    Expense.destroy({ where: { id: expenseId, userId: req.user.id } })
-        .then(noOfRows => {
-            if (noOfRows === 0) {
-                return res.status(404).json({ success: false, message: "Expense doesn't belong to the user" });
-            }
-            return res.status(200).json({ success: true, message: 'Expense deleted successfully' });
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ success: false, error: 'Failed to delete expense' });
-        });
+    try {
         
+        const expense = await Expense.findOne({
+            where: { id: expenseId, userId: req.user.id }
+        });
+
+        if (!expense) {
+            return res.status(404).json({ success: false, message: "Expense doesn't belong to the user" });
+        }
+
+        const amountToDelete = expense.amount;
+
+        
+        const t = await sequelize.transaction();
+
+        
+        await Expense.destroy({ where: { id: expenseId, userId: req.user.id }, transaction: t });
+
+        
+        const updatedTotalExpenses = Number(req.user.totalexpenses) - Number(amountToDelete);
+        await User.update(
+            { totalexpenses: updatedTotalExpenses },
+            { where: { id: req.user.id }, transaction: t }
+        );
+
+        
+        await t.commit();
+
+        return res.status(200).json({ success: true, message: 'Expense deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        if (t) {
+            await t.rollback();
+        }
+        return res.status(500).json({ success: false, error: 'Failed to delete expense' });
+    }
 };
+
 
 module.exports = {
     deleteExpense,
--- controllers/purchase.js
+++ controllers/purchase.js
@@ -2,37 +2,30 @@ const Order = require('../models/orders');
 const Razorpay = require('razorpay');
 const userController = require('../controllers/user');
 
-
 const purchasePremium = async (req, res) => {
   try {
-    
-    var rzp = new Razorpay({
+    const rzp = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_KEY_SECRET
     });
 
     const amount = 2500;
 
-    rzp.orders.create({ amount, currency: 'INR' }, (err, order) => {
-      if (err) {
-        throw new Error(JSON.stringify(err));
-      }
-
-      
-      req.user.createOrder({
-        
-        orderid: order.id,
-        status: 'PENDING'
-        
-      }).then(() => {
-        return res.status(201).json({ order, key_id: rzp.key_id });
-      }).catch((err) => {
-        throw new Error(err);
+    const order = await new Promise((resolve, reject) => {
+      rzp.orders.create({ amount, currency: 'INR' }, (err, order) => {
+        if (err) {
+          reject(new Error(JSON.stringify(err)));
+        } else {
+          resolve(order);
+        }
       });
     });
+
+    await req.user.createOrder({ orderid: order.id, status: 'PENDING' });
+    return res.status(201).json({ order, key_id: rzp.key_id });
   } catch (err) {
-    console.log(err);
-    res.status(403).json({ error: err, message: 'Something went wrong' });
+    console.error(err);
+    res.status(403).json({ error: err.message, message: 'Something went wrong' });
   }
 };
 
@@ -41,22 +34,16 @@ const updateTransactionStatus = async (req, res) => {
     const userId = req.user.id;
     const { payment_id, order_id } = req.body;
 
-      const order = await Order.findOne({ where: { orderid: order_id } })
-      const promise1 = order.update({ paymentid: payment_id, status: 'SUCCESSFUL' })
-      const promise2 = req.user.update({ispremiumuser:true})
+    const order = await Order.findOne({ where: { orderid: order_id } });
+    await Promise.all([
+      order.update({ paymentid: payment_id, status: 'SUCCESSFUL' }),
+      req.user.update({ ispremiumuser: true })
+    ]);
 
-      Promise.all([promise1,promise2]).then(()=>{
-        
-        
-        return res.status(202).json({ success: true, message: 'Transaction successful', token: userController.generateAccessToken(userId, undefined, true)});
-      }).catch((error) => {
-        throw new Error(error)
-      })
-        
-    }
-   catch (err) {
-    console.log(err);
-    res.status(404).json({ error: err, message: 'Something went wrong' });
+    return res.status(202).json({ success: true, message: 'Transaction successful', token: userController.generateAccessToken(userId, undefined, true)});
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ error: err.message, message: 'Something went wrong' });
   }
 };
 
@@ -64,3 +51,4 @@ module.exports = {
   purchasePremium,
   updateTransactionStatus
 };
+

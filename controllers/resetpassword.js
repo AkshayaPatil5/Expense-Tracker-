c35f591e17feeba462fa8ddfb9d7d1bd430afb73
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-10-31 16:00:26 +0530

    forgotpass

--- controllers/resetpassword.js
+++ controllers/resetpassword.js
@@ -49,74 +49,9 @@ const forgotpassword = async (req, res) => {
 
 }
 
-const resetpassword = (req, res) => {
-    const id =  req.params.id;
-    Forgotpassword.findOne({ where : { id }}).then(forgotpasswordrequest => {
-        if(forgotpasswordrequest){
-            forgotpasswordrequest.update({ active: false});
-            res.status(200).send(`<html>
-                                    <script>
-                                        function formsubmitted(e){
-                                            e.preventDefault();
-                                            console.log('called')
-                                        }
-                                    </script>
 
-                                    <form action="/password/updatepassword/${id}" method="get">
-                                        <label for="newpassword">Enter New password</label>
-                                        <input name="newpassword" type="password" required></input>
-                                        <button>reset password</button>
-                                    </form>
-                                </html>`
-                                )
-            res.end()
-
-        }
-    })
-}
-
-const updatepassword = (req, res) => {
-
-    try {
-        const { newpassword } = req.query;
-        const { resetpasswordid } = req.params;
-        Forgotpassword.findOne({ where : { id: resetpasswordid }}).then(resetpasswordrequest => {
-            User.findOne({where: { id : resetpasswordrequest.userId}}).then(user => {
-                // console.log('userDetails', user)
-                if(user) {
-                    //encrypt the password
-
-                    const saltRounds = 10;
-                    bcrypt.genSalt(saltRounds, function(err, salt) {
-                        if(err){
-                            console.log(err);
-                            throw new Error(err);
-                        }
-                        bcrypt.hash(newpassword, salt, function(err, hash) {
-                            // Store hash in your password DB.
-                            if(err){
-                                console.log(err);
-                                throw new Error(err);
-                            }
-                            user.update({ password: hash }).then(() => {
-                                res.status(201).json({message: 'Successfuly update the new password'})
-                            })
-                        });
-                    });
-            } else{
-                return res.status(404).json({ error: 'No user Exists', success: false})
-            }
-            })
-        })
-    } catch(error){
-        return res.status(403).json({ error, success: false } )
-    }
-
-}
 
 
 module.exports = {
-    forgotpassword,
-    updatepassword,
-    resetpassword
+    forgotpassword
 }
--- routes/resetpassword.js
+++ routes/resetpassword.js
@@ -7,9 +7,7 @@ const resetpasswordController = require('../controllers/resetpassword');
 
 const router = express.Router();
 
-router.get('/updatepassword/:resetpasswordid', resetpasswordController.updatepassword)
 
-router.get('/resetpassword/:id', resetpasswordController.resetpassword)
 
 router.use('/forgotpassword', resetpasswordController.forgotpassword)
 
